2ac246cd4e79825b917e14d9ec7be98b89307cc6
RamiOkkeh <ramiokkeh@gmail.com>
2020-12-05 15:44:40 +0200

    little front end refractoring

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,50 +1,47 @@
-import React from 'react';
-// import logo from './logo.svg';
-import './App.css';
-import SignUp from './Signup/Signup'
-import { connect } from 'react-redux'
-import { State } from './reducers/rootReducer'
-import { Dispatch } from 'redux';
-import { changeState } from './actions/actions'
+import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
+import { changeState } from "./actions/actions";
+import Header from "./components/Header/Header";
+import { State } from "./reducers/rootReducer";
+import SignUp from "./pages/Signup/Signup";
+import SignIn from "./pages/Signin/Signin";
+import Home from "./pages/Home/Home";
+import { connect } from "react-redux";
+import { Dispatch } from "redux";
+import React from "react";
+import "./App.css";
 
 interface Props {
   test23: Array<String>;
-  changeState: Function
+  changeState: Function;
 }
-
 
 const App: React.FC<Props> = ({ test23, changeState }) => {
-
-  const handleClick = () => {
-    console.log('clicked');
-    console.log(changeState);
-    console.log(test23);
-    changeState(['works?'])
-    console.log(test23);
-  }
   return (
     <div className="App">
-      <SignUp />
-      {test23.map((test, i) => {
-        return <div>{test}{i}</div>
-      })}
-      <button onClick={handleClick}>change state</button>
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/signup" component={SignUp} />
+          <Route path="/signin" component={SignIn} />
+        </Switch>
+      </Router>
     </div>
   );
-
-}
-
+};
 
 const mapDispatchToProps = (dispatch: Dispatch) => {
   return {
-    changeState: (newState: State) => { dispatch(changeState(newState)) }
-  }
-}
+    changeState: (newState: State) => {
+      dispatch(changeState(newState));
+    },
+  };
+};
 
 const mapStateToProps = (state: State) => {
   return {
-    test23: state.test
-  }
-}
+    test23: state.test,
+  };
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
