3183a140b617f4aaa9e640210ff00e1f7e19f577
ameedasmah <67121588+ameedasmah@users.noreply.github.com>
2020-12-09 16:46:21 +0200

    scheduels for student and Principal

---
+++ frontend/src/pages/schedulePage/SchedulePage.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Schedule from '../../components/schedule/Schedule'
+import SchedulePrincipal from '../../components/schedule/SchedulePrincipal'
+import {connect} from "react-redux";
+import { State } from "../../reducers/rootReducer";
+
+ function SchedulePage({role}:any) {
+    return ( 
+
+        <div>
+            { role ==="Student" ?
+            <Schedule />  :
+            role ==="Principal" ?
+            <SchedulePrincipal /> :
+            <div> </div>
+            }
+            
+        </div>
+    )
+}
+
+const mapStateToProps = (state: State) => {
+    return {
+        role: state.role
+    };
+};
+export default connect(mapStateToProps)(SchedulePage);
