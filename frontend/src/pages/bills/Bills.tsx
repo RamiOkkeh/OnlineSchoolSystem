ab0226d1a9dad975285aac4fd1e83ae63f7b7589
ameedasmah <67121588+ameedasmah@users.noreply.github.com>
2020-12-12 19:55:47 +0200

    fixed bills conflict

--- frontend/src/pages/bills/Bills.tsx
+++ frontend/src/pages/bills/Bills.tsx
@@ -6,24 +6,24 @@ import { State } from "../../reducers/rootReducer"
 
 function Bills({ user }: any) {
     const [payments, setPayments] = useState([])
-    // useEffect(() => {
-    //     let options = {
-    //         method: "POST",
-    //         headers: { "Content-Type": "application/json" },
-    //         body: JSON.stringify({ studentID: 5 })
-    //     };
-    //     let path =
-    //         process.env.NODE_ENV === "production"
-    //             ? "/payment/"
-    //             : "http://localhost:8000/payment/details";
-    //     fetch(path, options)
-    //         .then((data) => data.json())
-    //         .then((data) => {
-    //             console.log(data);
-    //             setPayments(data)
-    //         });
-    // }, [])
-    console.log('ameed',user);
+    useEffect(() => {
+        let options = {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ studentID: 5 })
+        };
+        let path =
+            process.env.NODE_ENV === "production"
+                ? "/payment/"
+                : "http://localhost:8000/payment/details";
+        fetch(path, options)
+            .then((data) => data.json())
+            .then((data) => {
+                console.log(data);
+                setPayments(data)
+            });
+    }, [])
+    console.log(payments);
     return (
         <div className="Bills">
             <div className="main">
@@ -35,9 +35,9 @@ function Bills({ user }: any) {
                 payments.map((el, key) => (
                     <div>
                         <div className="main">
-                            <div className="firstBox"> {user.date} </div>
-                            <div className="secondtBox"> {user.semester}</div>
-                            <div className="thirdtBox"> {user.totalprice}</div>
+                            <div className="firstBox"> {el['dueDate']} </div>
+                            <div className="secondtBox"> {el['semester']}</div>
+                            <div className="thirdtBox"> {el['amount']}</div>
                         </div>
 
                         {
@@ -47,7 +47,7 @@ function Bills({ user }: any) {
                     </div>
                 ))
             }
-            <StripButton />
+            {/* <StripButton /> */}
         </div>
     )
 }
