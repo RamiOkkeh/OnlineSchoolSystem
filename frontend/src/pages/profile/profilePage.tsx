e6adb09764c7405e4f28e2910cbc79b44ef872d0
ameedasmah <67121588+ameedasmah@users.noreply.github.com>
2020-12-14 17:53:54 +0200

    fixing conflict

--- frontend/src/pages/profile/profilePage.tsx
+++ frontend/src/pages/profile/profilePage.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import React from 'react';
 import Profile from "../../components/profile/profileStudent"
 import ProfileTeacher from "../../components/profile/profileTeacher"
