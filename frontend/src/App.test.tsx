c7ca9a6ebc7d93351e18285b8502611f902e87e5
RamiOkkeh <ramiokkeh@gmail.com>
2020-12-22 16:03:55 +0200

    test

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
-test("tes", () => {
-  expect(1).toEqual(1);
+test("renders learn react link", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
+// test("test", () => {
+//   expect(1).toEqual(1);
+// });
--- frontend/src/ml5Training.test.js
+++ frontend/src/ml5Training.test.js
@@ -1,19 +1,19 @@
 import model from "./ml5Training";
 
-// test("should label a student with good grades with 'ok'", async () => {
-//   let studentStatus;
-//   await model([95, 89, 94, 80, 92], (result) => {
-//     studentStatus = result;
-//   });
-//   expect(studentStatus.label).toEqual("ok");
-// });
-// test("should label a student with dropping grades with 'depressed'", async () => {
-//   let studentStatus;
-//   await model([95, 89, 94, 72, 64], (result) => {
-//     studentStatus = result;
-//   });
-//   expect(studentStatus.label).toEqual("depressed");
-// });
-test("test", () => {
-  expect(1).toEqual(1);
+test("should label a student with good grades with 'ok'", async () => {
+  let studentStatus;
+  await model([95, 89, 94, 80, 92], (result) => {
+    studentStatus = result;
+  });
+  expect(studentStatus.label).toEqual("ok");
 });
+test("should label a student with dropping grades with 'depressed'", async () => {
+  let studentStatus;
+  await model([95, 89, 94, 72, 64], (result) => {
+    studentStatus = result;
+  });
+  expect(studentStatus.label).toEqual("depressed");
+});
+// test("test", () => {
+//   expect(1).toEqual(1);
+// });
